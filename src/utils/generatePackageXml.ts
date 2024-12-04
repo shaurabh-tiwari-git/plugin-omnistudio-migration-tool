56ad3b7c206543222c2e380a47d4c6d967daaee6
abhinavkumar2 <abhinavkumar2@salesforce.com>
2024-12-04 09:48:44 +0530

    fix: fixed packageXml generation

--- src/utils/generatePackageXml.ts
+++ src/utils/generatePackageXml.ts
@@ -38,6 +38,7 @@ export class generatePackageXml {
 <Package xmlns="http://soap.sforce.com/2006/04/metadata">
       ${apexXml}
       ${lwcXml}
+      ${additionalTypes}
     <version>57.0</version>
 </Package>
 `;
