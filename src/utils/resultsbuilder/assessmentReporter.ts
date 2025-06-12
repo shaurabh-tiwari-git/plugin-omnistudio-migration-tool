f19322292991f251e1453105d5ecd6dc92df0ab8
shaurabh-tiwari-git <shaurabh.tiwari@salesforce.com>
2025-06-12 16:20:13 +0530

    @W-18670330 @W-18670348 Apex and LWC reports framework (#279)

--- src/utils/resultsbuilder/assessmentReporter.ts
+++ src/utils/resultsbuilder/assessmentReporter.ts
@@ -3,16 +3,12 @@
 import fs from 'fs';
 import path from 'path';
 import open from 'open';
-import {
-  ApexAssessmentInfo,
-  AssessmentInfo,
-  LWCAssessmentInfo,
-  FlexCardAssessmentInfo,
-  nameLocation,
-} from '../interfaces';
+import { AssessmentInfo, FlexCardAssessmentInfo, nameLocation } from '../interfaces';
 import { ReportHeaderFormat } from '../reportGenerator/reportInterfaces';
 import { OmnistudioOrgDetails } from '../orgUtils';
 import { OSAssessmentReporter } from './OSAssessmentReporter';
+import { LWCAssessmentReporter } from './LWCAssessmentReporter';
+import { ApexAssessmentReporter } from './ApexAssessmentReporter';
 import { IPAssessmentReporter } from './IPAssessmentReporter';
 import { DRAssessmentReporter } from './DRAssessmentReporter';
 
@@ -49,9 +45,16 @@ export class AssessmentReporter {
       dataMapperAssessmentFilePath,
       DRAssessmentReporter.generateDRAssesment(result.dataRaptorAssessmentInfos, instanceUrl, orgDetails)
     );
-    this.createDocument(apexAssessmentFilePath, this.generateApexAssesment(result.apexAssessmentInfos));
-    // TODO: Uncomment code once MVP for migration is completed
-    // this.createDocument(lwcAssessmentFilePath, this.generateLwcAssesment(result.lwcAssessmentInfos));
+
+    this.createDocument(
+      apexAssessmentFilePath,
+      ApexAssessmentReporter.generateApexAssesment(result.apexAssessmentInfos, instanceUrl, orgDetails)
+    );
+    this.createDocument(
+      lwcAssessmentFilePath,
+      LWCAssessmentReporter.generateLwcAssesment(result.lwcAssessmentInfos, instanceUrl, orgDetails)
+    );
+
     const nameUrls = [
       {
         name: 'omnscript assessment report',
@@ -188,94 +191,6 @@ export class AssessmentReporter {
     const doc = this.generateDocument(htmlBody);
     fs.writeFileSync(filePath, doc);
   }
-  // @ts-expect-error - LWC functionality temporarily disabled
-  private static generateLwcAssesment(lwcAssessmentInfos: LWCAssessmentInfo[]): string {
-    let tableBody = '';
-    tableBody += `
-    <html>
-            <head>
-                <title>OmniStudio Migration Assessment</title>
-                <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/design-system/2.17.5/styles/salesforce-lightning-design-system.min.css" />
-            </head>
-            <body>
-            <div style="margin: 20px;">
-                <div class="slds-text-heading_large">OmniStudio Migration Assessment</div>`;
-    tableBody += '<div class="slds-text-heading_large">LWC Assessment</div>';
-    for (const lwcAssessmentInfo of lwcAssessmentInfos) {
-      let changeInfoRows = '';
-
-      for (const changeInfo of lwcAssessmentInfo.changeInfos) {
-        changeInfoRows += `<tr class ="slds-hint_parent">
-                                <td><div class="slds-truncate" title="${changeInfo.name}"><a href="${changeInfo.path}">${changeInfo.name}</div></td>
-                                <td><div class="slds-scrollable" style="height:8rem;width:36rem"><pre>${changeInfo.diff}<pre></div></td>
-                            </tr>`;
-      }
-      const changeInfoTable = `<table>
-                                    ${changeInfoRows}
-                                </table>`;
-      const row = `<tr class="slds-hint_parent">
-                            <td><div class="slds-truncate" title="${lwcAssessmentInfo.name}">${lwcAssessmentInfo.name}</div></td>
-                            <td>${changeInfoTable}</td>
-                        </tr>`;
-      tableBody += row;
-    }
-    tableBody += `
-    </div>
-            </div>
-            </body>
-        </html>
-        `;
-    return this.getLWCAssesmentReport(tableBody);
-  }
-
-  private static generateApexAssesment(apexAssessmentInfos: ApexAssessmentInfo[]): string {
-    let tableBody = '';
-    tableBody += '<div class="slds-text-heading_large">Apex Assessment</div>';
-    for (const apexAssessmentInfo of apexAssessmentInfos) {
-      const message = this.generateMessages(apexAssessmentInfo.infos);
-      const errors = this.generateMessages(apexAssessmentInfo.warnings);
-      const row = `<tr class="slds-hint_parent">
-      <td><div class="slds-truncate" title="${apexAssessmentInfo.name}">${apexAssessmentInfo.name}</div></td>
-      <td><div class="slds-truncate" title="${apexAssessmentInfo.name}"><a href="${apexAssessmentInfo.path}">${apexAssessmentInfo.name}</div></td>
-      <td><div class="slds-truncate">${apexAssessmentInfo.diff}</div></td>
-      <td><div class="slds-truncate"></div>${message}</td>
-      <td><div class="slds-truncate"></div>${errors}</td>
-     </tr>`;
-      tableBody += row;
-    }
-    return this.getApexAssessmentReport(tableBody);
-  }
-
-  private static getApexAssessmentReport(tableContent: string): string {
-    const tableBody = `
-      <div style="margin-block:15px">        
-        <table class="slds-table slds-table_cell-buffer slds-table_bordered slds-table_striped slds-table_col-bordered" aria-label="Results for Apex updates">
-        <thead>
-            <tr class="slds-line-height_reset">
-                <th class="" scope="col" style="width: 25%">
-                    <div class="slds-truncate" title="Name">Name</div>
-                </th>
-                <th class="" scope="col" style="width: 10%">
-                    <div class="slds-truncate" title="File">File reference</div>
-                </th>
-                <th class="" scope="col" style="width: 10%">
-                    <div class="slds-truncate" title="Diff">Diff</div>
-                </th>
-                <th class="" scope="col" style="width: 10%">
-                    <div class="slds-truncate" title="Infos">Comments</div>
-                </th>
-                <th class="" scope="col" style="width: 10%">
-                    <div class="slds-truncate" title="Warnings">Errors</div>
-                </th>
-            </tr>
-        </thead>
-        <tbody>
-        ${tableContent}
-        </tbody>
-        </table>
-      </div>`;
-    return tableBody;
-  }
 
   private static generateCardAssesment(flexCardAssessmentInfos: FlexCardAssessmentInfo[], instanceUrl: string): string {
     let tableBody = '';
@@ -303,14 +218,6 @@ export class AssessmentReporter {
     }
 
     return this.getCardAssessmentReport(tableBody);
-  }
-
-  private static generateMessages(messages: string[]): string {
-    let messageBody = '';
-    for (const message of messages) {
-      messageBody += `<li class="slds-item slds-text-color_destructive">${message}</li>`;
-    }
-    return messageBody;
   }
 
   private static generateDocument(resultsAsHtml: string): string {
@@ -359,31 +266,6 @@ export class AssessmentReporter {
             </tbody>
             </table>
         </div>`;
-    return tableBody;
-  }
-
-  private static getLWCAssesmentReport(tableContent: string): string {
-    const tableBody = `
-      <div style="margin-block:15px">
-        <table class="slds-table slds-table_cell-buffer slds-table_bordered slds-table_striped slds-table_col-bordered" aria-label="Results for LWC updates">
-        <thead>
-            <tr class="slds-line-height_reset">
-                <th class="" scope="col" style="width: 25%">
-                    <div class="slds-truncate" title="Name">Name</div>
-                </th>
-                <th class="" scope="col" style="width: 10%">
-                    <div class="slds-truncate" title="Changes">File Path & Diff</div>
-                </th>
-                <th class="" scope="col">
-                    <div class="slds-truncate" title="Errors">Errors</div>
-                </th>
-            </tr>
-        </thead>
-        <tbody>
-        ${tableContent}
-        </tbody>
-        </table>
-      </div>`;
     return tableBody;
   }
 }
