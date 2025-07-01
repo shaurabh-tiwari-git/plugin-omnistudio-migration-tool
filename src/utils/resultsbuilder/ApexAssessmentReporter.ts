adc094673325083249e0b380378ec24f23d42a0a
shaurabh-tiwari <shaurabh.tiwari@salesforce.com>
2025-07-01 16:50:18 +0530

    feat: added the diff modal

--- src/utils/resultsbuilder/ApexAssessmentReporter.ts
+++ src/utils/resultsbuilder/ApexAssessmentReporter.ts
@@ -9,6 +9,7 @@ import {
 } from '../reportGenerator/reportInterfaces';
 import { createFilterGroupParam, createRowDataParam, getOrgDetailsForReport } from '../reportGenerator/reportUtil';
 import { reportingHelper } from './reportingHelper';
+import { FileDiffUtil } from '../lwcparser/fileutils/FileDiffUtil';
 
 export class ApexAssessmentReporter {
   private static rowId = 0;
@@ -53,7 +54,7 @@ export class ApexAssessmentReporter {
       data: [
         createRowDataParam('name', apexAssessmentInfo.name, true, 1, 1, false),
         createRowDataParam('fileReference', apexAssessmentInfo.path, false, 1, 1, false),
-        createRowDataParam('diff', apexAssessmentInfo.diff, false, 1, 1, false),
+        createRowDataParam('diff', apexAssessmentInfo.name+'diff', false, 1, 1, false, undefined, FileDiffUtil.getDiffHTML(apexAssessmentInfo.diff, apexAssessmentInfo.name)),
         createRowDataParam(
           'comments',
           apexAssessmentInfo.infos ? apexAssessmentInfo.infos.join(', ') : '',
