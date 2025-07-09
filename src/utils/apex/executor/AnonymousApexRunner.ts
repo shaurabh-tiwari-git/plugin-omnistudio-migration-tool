06fd123a6abf0346871f30fb7d09fd49af9c7e52
Rajender Kumar <rajender.kumar@salesforce.com>
2025-07-09 20:26:15 +0530

    feat: migrate global auto number

--- src/utils/apex/executor/AnonymousApexRunner.ts
+++ src/utils/apex/executor/AnonymousApexRunner.ts
@@ -1,8 +1,12 @@
-import { Org } from '@salesforce/core';
+import { Org, Connection } from '@salesforce/core';
 import { ExecuteAnonymousResult } from 'jsforce';
 
 export class AnonymousApexRunner {
   public static async run(org: Org, anonymousApex: string): Promise<ExecuteAnonymousResult> {
     return org.getConnection().tooling.executeAnonymous(anonymousApex);
+  }
+
+  public static async runWithConnection(connection: Connection, anonymousApex: string): Promise<ExecuteAnonymousResult> {
+    return connection.tooling.executeAnonymous(anonymousApex);
   }
 }
